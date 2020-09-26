de117fde39fb78486023b3723e4af54f9161063d
Sathvik713 <70425727+Sathvik713@users.noreply.github.com>
2020-09-26 12:45:36 +0530

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,31 @@
+class Slingshot{
+    constructor(bodyA, pointb){
+        var options = {
+            bodyA: bodyA,
+            pointB: pointb,
+            stiffness: 0.04,
+            length: 10
+        }
+            this.pointb=pointb;            
+
+        this.sling = Constraint.create(options);
+        World.add(world, this.sling);
+    }
+    fly(){
+        this.sling.bodyA=null;
+
+    }
+
+    display(){
+        if(this.sling.bodyA){
+
+
+        
+        var pointA = this.sling.bodyA.position;
+        var pointB = this.pointb;
+
+        strokeWeight(4);
+        line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+ }
+}
